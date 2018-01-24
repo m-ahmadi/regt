d77c6eb30bcfd7344bf59e5475907f643e46da8d
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2018-01-24 13:52:57 +0330

    5

--- compile-icons.bat
+++ compile-icons.bat
@@ -1,5 +1,9 @@
+if exist dist\uikit-icons.js (
+	del dist\uikit-icons.js /F /Q
+)
 rd uk\custom\icons /S /Q
 robocopy src\icons uk\custom\icons /E /NS /NC /NFL /NDL /NP /NJH /NJS
 cd uk
 cmd /c npm run compile
 robocopy dist/js ../dist uikit-icons.js /NS /NC /NFL /NDL /NP /NJH /NJS
+cd ../
